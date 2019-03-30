f9c3a845bc0fc0f179ecf2562bc96d798ca43a56
JulienRoy <jul.roy1311@gmail.com>
2019-03-30 12:01:17 -0400

    Copied content of a2c.py in oc.py. After some more examination of the code, I think it will be easier to start from torch_rl's implementation of A2C and modify it towards the Option-Critic algorithm than to modify DeepRL's implementation of OptionCritic and make it fit into torch_rl's codebase.

--- torch_rl/torch_rl/algos/oc.py
+++ torch_rl/torch_rl/algos/oc.py
@@ -1,115 +1,112 @@
-#######################################################################
-# Copyright (C) 2017 Shangtong Zhang(zhangshangtong.cpp@gmail.com)    #
-# Permission given to modify the code as long as you keep this        #
-# declaration at the top                                              #
-#######################################################################
+import numpy
+import torch
+import torch.nn.functional as F
 
-from ..network import *
-from .BaseAgent import *
+from torch_rl.algos.base import BaseAlgo
 
-class OptionCriticAgent(BaseAgent):
-    def __init__(self, config):
-        BaseAgent.__init__(self, config)
-        self.config = config
-        self.task = config.task_fn()
-        self.network = config.network_fn()
-        self.target_network = config.network_fn()
-        self.optimizer = config.optimizer_fn(self.network.parameters())
-        self.target_network.load_state_dict(self.network.state_dict())
+class OCAlgo(BaseAlgo):
+    """The class for the Advantage Actor-Critic algorithm."""
 
-        self.online_rewards = np.zeros(config.num_workers)
-        self.episode_rewards = []
+    def __init__(self, envs, acmodel, num_frames_per_proc=None, discount=0.99, lr=7e-4, gae_lambda=0.95,
+                 entropy_coef=0.01, value_loss_coef=0.5, max_grad_norm=0.5, recurrence=4,
+                 rmsprop_alpha=0.99, rmsprop_eps=1e-5, preprocess_obss=None, reshape_reward=None):
+        num_frames_per_proc = num_frames_per_proc or 8
 
-        self.total_steps = 0
-        s0 = self.task.reset()
-        states = self.config.state_normalizer(self.task.reset())
-        self.q_options, self.betas, self.log_pi = self.network(states)
-        self.options = epsilon_greedy(config.random_option_prob(config.num_workers), to_np(self.q_options))
-        self.is_initial_betas = np.ones(self.config.num_workers)
-        self.prev_options = np.copy(self.options)
+        super().__init__(envs, acmodel, num_frames_per_proc, discount, lr, gae_lambda, entropy_coef,
+                         value_loss_coef, max_grad_norm, recurrence, preprocess_obss, reshape_reward)
 
-    def step(self):
-        config = self.config
-        rollout = []
+        self.optimizer = torch.optim.RMSprop(self.acmodel.parameters(), lr,
+                                             alpha=rmsprop_alpha, eps=rmsprop_eps)
 
-        q_options, betas, options, log_pi = self.q_options, self.betas, self.options, self.log_pi
-        for _ in range(config.rollout_length):
-            var_options = tensor(options).long()
-            worker_index = tensor(np.arange(config.num_workers)).long()
-            intra_log_pi = log_pi[worker_index, var_options, :]
-            dist = torch.distributions.Categorical(intra_log_pi.exp())
-            actions = dist.sample()
-            next_states, rewards, terminals, _ = self.task.step(actions.cpu().detach().numpy().flatten())
-            next_states = config.state_normalizer(next_states)
-            self.online_rewards += rewards
-            rewards = config.reward_normalizer(rewards)
-            q_options_next, betas_next, log_pi_next = self.network(next_states)
-            rollout.append([q_options, betas, options, self.prev_options, rewards, 1 - terminals, np.copy(self.is_initial_betas), intra_log_pi, actions])
-            self.is_initial_betas = np.asarray(terminals, dtype=np.float32)
+    def update_parameters(self):
+        # Collect experiences
 
-            np_q_options_next = q_options_next.cpu().detach().numpy()
-            np_betas_next = betas_next.gather(1, var_options.unsqueeze(1)).cpu().detach().numpy().flatten()
-            options_next = np.copy(options)
-            dice = np.random.rand(len(options_next))
-            epsilon = config.random_option_prob(config.num_workers)
-            for j in range(len(dice)):
-                if dice[j] < np_betas_next[j] or terminals[j]:
-                    options_next[j] = epsilon_greedy(epsilon, np_q_options_next[j])
-            for i, terminal in enumerate(terminals):
-                if terminals[i]:
-                    self.episode_rewards.append(self.online_rewards[i])
-                    self.online_rewards[i] = 0
-            self.prev_options = options
-            options = options_next
-            q_options = q_options_next
-            betas = betas_next
-            log_pi = log_pi_next
+        exps, logs = self.collect_experiences()
 
-            self.total_steps += config.num_workers
-            if self.total_steps / config.num_workers % config.target_network_update_freq == 0:
-                self.target_network.load_state_dict(self.network.state_dict())
+        # Compute starting indexes
 
-        self.options = options
-        self.q_options = q_options
-        self.betas = betas
-        self.log_pi = log_pi
+        inds = self._get_starting_indexes()
 
-        target_q_options, _, _ = self.target_network(next_states)
-        prev_options = tensor(self.prev_options).long().unsqueeze(1)
-        betas_prev_options = betas.gather(1, prev_options)
+        # Initialize update values
 
-        returns = (1 - betas_prev_options) * target_q_options.gather(1, prev_options) +\
-                  betas_prev_options * torch.max(target_q_options, dim=1, keepdim=True)[0]
-        returns = returns.detach()
+        update_entropy = 0
+        update_value = 0
+        update_policy_loss = 0
+        update_value_loss = 0
+        update_loss = 0
 
-        processed_rollout = [None] * (len(rollout))
-        for i in reversed(range(len(rollout))):
-            q_options, betas, options, prev_options, rewards, terminals, is_initial_betas, log_pi, actions = rollout[i]
-            options = tensor(options).unsqueeze(1).long()
-            prev_options = tensor(prev_options).unsqueeze(1).long()
-            terminals = tensor(terminals).unsqueeze(1)
-            rewards = tensor(rewards).unsqueeze(1)
-            is_initial_betas = tensor(is_initial_betas).unsqueeze(1)
-            returns = rewards + config.discount * terminals * returns
+        # Initialize memory
 
-            q_omg = q_options.gather(1, options)
-            log_action_prob = log_pi.gather(1, actions.unsqueeze(1))
-            entropy_loss = (log_pi.exp() * log_pi).sum(-1).unsqueeze(1)
+        if self.acmodel.recurrent:
+            memory = exps.memory[inds]
 
-            q_prev_omg = q_options.gather(1, prev_options)
-            v_prev_omg = torch.max(q_options, dim=1, keepdim=True)[0]
-            advantage_omg = q_prev_omg - v_prev_omg
-            advantage_omg.add_(config.termination_regularizer)
-            betas = betas.gather(1, prev_options)
-            betas = betas * (1 - is_initial_betas)
-            processed_rollout[i] = [q_omg, returns, betas, advantage_omg.detach(), log_action_prob, entropy_loss]
+        for i in range(self.recurrence):
+            # Create a sub-batch of experience
 
-        q_omg, returns, beta_omg, advantage_omg, log_action_prob, entropy_loss = map(lambda x: torch.cat(x, dim=0), zip(*processed_rollout))
-        pi_loss = -log_action_prob * (returns - q_omg.detach()) + config.entropy_weight * entropy_loss
-        pi_loss = pi_loss.mean()
-        q_loss = 0.5 * (q_omg - returns).pow(2).mean()
-        beta_loss = (advantage_omg * beta_omg).mean()
+            sb = exps[inds + i]
+
+            # Compute loss
+
+            if self.acmodel.recurrent:
+                dist, value, memory = self.acmodel(sb.obs, memory * sb.mask)
+            else:
+                dist, value = self.acmodel(sb.obs)
+
+            entropy = dist.entropy().mean()
+
+            policy_loss = -(dist.log_prob(sb.action) * sb.advantage).mean()
+
+            value_loss = (value - sb.returnn).pow(2).mean()
+
+            loss = policy_loss - self.entropy_coef * entropy + self.value_loss_coef * value_loss
+
+            # Update batch values
+
+            update_entropy += entropy.item()
+            update_value += value.mean().item()
+            update_policy_loss += policy_loss.item()
+            update_value_loss += value_loss.item()
+            update_loss += loss
+
+        # Update update values
+
+        update_entropy /= self.recurrence
+        update_value /= self.recurrence
+        update_policy_loss /= self.recurrence
+        update_value_loss /= self.recurrence
+        update_loss /= self.recurrence
+
+        # Update actor-critic
+
         self.optimizer.zero_grad()
-        (pi_loss + q_loss + beta_loss).backward()
-        nn.utils.clip_grad_norm_(self.network.parameters(), config.gradient_clip)
+        update_loss.backward()
+        update_grad_norm = sum(p.grad.data.norm(2) ** 2 for p in self.acmodel.parameters()) ** 0.5
+        torch.nn.utils.clip_grad_norm_(self.acmodel.parameters(), self.max_grad_norm)
         self.optimizer.step()
+
+        # Log some values
+
+        logs["entropy"] = update_entropy
+        logs["value"] = update_value
+        logs["policy_loss"] = update_policy_loss
+        logs["value_loss"] = update_value_loss
+        logs["grad_norm"] = update_grad_norm
+
+        return logs
+
+    def _get_starting_indexes(self):
+        """Gives the indexes of the observations given to the model and the
+        experiences used to compute the loss at first.
+
+        The indexes are the integers from 0 to `self.num_frames` with a step of
+        `self.recurrence`. If the model is not recurrent, they are all the
+        integers from 0 to `self.num_frames`.
+
+        Returns
+        -------
+        starting_indexes : list of int
+            the indexes of the experiences to be used at first
+        """
+
+        starting_indexes = numpy.arange(0, self.num_frames, self.recurrence)
+        return starting_indexes
