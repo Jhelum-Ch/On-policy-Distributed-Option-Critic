8993477957c2106b6f23434ed4f7e5cbae8e1b82
JulienRoy <jul.roy1311@gmail.com>
2019-04-17 18:32:36 -0400

    Fixed get_actions in agent.py to work with options

--- utils/agent.py
+++ utils/agent.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 import utils
 
@@ -7,40 +8,55 @@ class Agent:
     - to choose an action given an observation,
     - to analyze the feedback (i.e. reward and done state) of its action."""
 
-    def __init__(self, env_id, obs_space, model_dir, argmax=False, num_envs=1, num_options=1):
+    def __init__(self, env_id, obs_space, model_dir, argmax=False, num_envs=1):
         _, self.preprocess_obss = utils.get_obss_preprocessor(env_id, obs_space, model_dir)
         self.acmodel = utils.load_model(model_dir)
+        assert self.acmodel.recurrent  # just to avoid supporting both
         self.argmax = argmax
         self.num_envs = num_envs
-        self.num_options = num_options
-        self.curr_option = None
+        self.num_options = self.acmodel.num_options
+        self.curr_option = np.random.randint(self.num_options) if self.num_options is not None else None
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         if self.acmodel.recurrent:
             self.memories = torch.zeros(self.num_envs, self.acmodel.memory_size)
 
     def get_actions(self, obss):
+
+        # proprocess observations
+
         preprocessed_obss = self.preprocess_obss(obss)
 
+        # forward propagation
+
         with torch.no_grad():
-            if self.acmodel.recurrent and self.num_options is not None:
-                act_dist, _, memory, term_dist = self.acmodel(preprocessed_obss, self.memories)
-            elif self.acmodel.recurrent and not self.num_options is not None:
-                act_dist, _, memory = self.acmodel(preprocessed_obss, self.memories)
-            elif not self.acmodel.recurrent and self.num_options is not None:
-                act_dist, _, term_dist = self.acmodel(preprocessed_obss)
+            if self.num_options is not None:
+                act_dist, act_values, memory, term_dist = self.acmodel(preprocessed_obss, self.memories)
             else:
-                act_dist, _ = self.acmodel(preprocessed_obss)
+                act_dist, _, memory = self.acmodel(preprocessed_obss)
+
+        if self.num_options is not None:
+
+            # option selection
+
+            terminate = term_dist.sample()[:, self.curr_option]
+            if terminate:
+                self.curr_option = int(torch.argmax(torch.sum(act_dist.probs * act_values, dim=-1), dim=-1))
+
+        # action selection
 
         if self.argmax:
-            actions = act_dist.probs.max(1, keepdim=True)[1]
+            action = act_dist.probs.max(axis=-1, keepdim=True)[1]
         else:
-            actions = act_dist.sample()
+            action = act_dist.sample()
+
+        if self.num_options is not None:
+            action = action[:, self.curr_option]
 
         if torch.cuda.is_available():
-            actions = actions.cpu().numpy()
+            action = action.cpu().numpy()
 
-        return actions
+        return action
 
     def get_action(self, obs):
         return self.get_actions([obs]).item()
