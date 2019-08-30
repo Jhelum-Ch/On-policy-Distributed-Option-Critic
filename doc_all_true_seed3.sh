d5cc973c011127e718b4d43242bb2d33f5e1b1c2
Nadeem Ward <wardnade@beluga1.int.ets1.calculquebec.ca>
2019-08-30 19:10:50 -0400

    few scripts

---
+++ doc_all_true_seed3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --account=def-bengioy                                                                            
+#SBATCH --time=10:00:00
+#SBATCH --mem=500G                                                                                       
+#SBATCH --cpus-per-task=40                                                                               
+#SBATCH --ntasks=1                               
+#SBATCH --job-name=doc_all_true3                                                        
+#SBATCH --output=/scratch/wardnade/maoc/%x-%j.out                                                        
+#SBATCH --mail-user=wardnade@mila.quebec                                                                 
+#SBATCH --mail-type=ALL
+
+module load python/3.6
+source $HOME/option-critic/bin/activate
+
+python scripts/train.py --seed=25 --use_critic=True --use_always_broadcast=True --use_teamgrid=True --desc='doc_all_true3' --algo='doc' --env='TEAMGrid-FourRooms-v0' --tb=False --num_options=3
+
