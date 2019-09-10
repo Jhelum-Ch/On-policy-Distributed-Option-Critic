674ea88ca428cac3a3fb2ccb9e5f1acdf4e28d9b
Nadeem Ward <patrick.ward@mail.mcgill.ca>
2019-09-10 11:25:47 -0400

    added new particle scripts

---
+++ particle_centralA2C_seed3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --account=def-bengioy
+#SBATCH --time=10:00:00
+#SBATCH --mem=500G
+#SBATCH --cpus-per-task=40
+#SBATCH --ntasks=1
+#SBATCH --job-name=centralA2C_particle
+#SBATCH --output=/scratch/wardnade/maoc/%x-%j.out
+#SBATCH --mail-user=wardnade@mila.quebec
+#SBATCH --mail-type=ALL
+
+module load python/3.6
+source $HOME/option-critic/bin/activate
+
+python scripts/train.py --seed=25 --use_central_critic=True --use_always_broadcast=False --use_teamgrid=False --desc='centralA2C_particle' --algo='a2c' --tb=False --num_options=1 --experiment_dir=1
---
+++ particle_doc_all_true_seed4.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --account=def-bengioy
+#SBATCH --time=10:00:00
+#SBATCH --mem=500G
+#SBATCH --cpus-per-task=40
+#SBATCH --ntasks=1
+#SBATCH --job-name=particle_doc_all_true
+#SBATCH --output=/scratch/wardnade/maoc/%x-%j.out
+#SBATCH --mail-user=wardnade@mila.quebec
+#SBATCH --mail-type=ALL
+
+module load python/3.6
+source $HOME/option-critic/bin/activate
+
+python scripts/train.py --seed=22 --use_central_critic=True --use_always_broadcast=True --use_teamgrid=False --desc='particle_doc_all_true' --algo='doc' --tb=False --num_options=3 --experiment_dir=1
