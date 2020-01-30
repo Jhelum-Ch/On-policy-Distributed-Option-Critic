bfed1228e3a66274d26c2b2b4e79f5f5d25c13f1
Nadeem Ward <patrick.ward@mail.mcgill.ca>
2020-01-30 11:19:16 -0500

    extra plotting things

---
+++ plots/trim_data.py
@@ -0,0 +1,236 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from pathlib import Path
+from os import path
+import argparse
+import os
+import sys
+import utils
+from utils.plots import *
+import pdb
+
+
+def get_plotting_args(overwritten_args=None):
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument("--json_file", default='plots/mean_returns_switch.json',
+                        help="the json file that specifies what experiments to plot")
+    parser.add_argument("--document_name", default="return", help="the name of the created png file")
+    parser.add_argument("--title", default='Average Returns', help='Title of the plot created')
+    parser.add_argument("--xlabel", default="Frames", help="Label of the x-axis")
+    parser.add_argument("--ylabel", default="reward", help="Label on the y-axis")
+
+    return parser.parse_args(overwritten_args)
+
+def check_experiment_length(xs):
+    '''
+    Function to check if the x-axis of all of the simulation wrt seeds within an experiment are of the same length
+
+    :param xs: list of list, where each sublist is the x-axis of that experiment
+    :return: False if they don't match. If they do match just return the common x-axis for all of the experiments (assuming same length means they have same elements)
+    '''
+
+    first_length = len(xs[0])
+    for x in xs:
+        if len(x) != first_length:
+            print("The length of the experiments don't match. One experiment has length {0} and another has length {1}.".format(first_length,len(x)))
+            return False
+
+    return xs[0]
+
+def match_length(ys, std_results, xs):
+    '''
+    Function to match the length of the different experiments. Idea is to loop through x-axis of smallest and match these elements with the x-axis of the others.
+    This deletes the elements that aren't common to both for the ys, std_dev and xs.
+
+    :param ys: List of list. Each sublist is the ys of that experiment
+    :param std_results: List of list. Each sublist is the std_devs of that experiment
+    :param xs: List of list. Each sublist is the x-axis of that experiment
+    :return: modified ys, std, xs such that they have the same length
+    '''
+
+    # Find Smallest
+    min_length = sys.maxsize
+    min_index = -1
+    for i in range(len(xs)):
+        if min_length > len(xs[i]):
+            min_length = len(xs[i])
+            min_index = i
+
+    several_min=[min_index]
+    for i in range(len(xs)):
+        if min_length == len(xs[i]) and i != min_index:
+            several_min.append(i)
+
+    # Loop through all others, and remove extra elements
+    for indx, x in enumerate(xs):
+
+        if indx != min_index:
+
+            counter = 0 # keep track of where we are at for the x[min_index]
+            x_to_remove = [] # list of xs that are extra, will be removed
+            y_indx_to_remove = [] # list of index positions for y's to be removed
+            for x_indx, element in enumerate(x):
+
+                # element of x not equal to x of min
+                if (element != xs[min_index][counter]):
+
+                    # keep track of element to be removed from x-axis (this assumes each entry is unique there append elements and not index)
+                    x_to_remove.append(element)
+                    # keep track of index to be removed from ys
+                    y_indx_to_remove.append(x_indx)
+
+                else:
+                    counter += 1
+
+            # remove all elements that where in the list of elements to be removed
+            xs[indx] = [e for e in x if e not in x_to_remove]
+
+            # remove extra ys
+            ys[indx] = np.delete(ys[indx], y_indx_to_remove)
+            ys[indx] = np.reshape(ys[indx], (ys[indx].shape[0], 1)) #reshape to be compatible
+
+            # same thing with std dev
+            std_results[indx] = np.delete(std_results[indx], y_indx_to_remove)
+            std_results[indx] = np.reshape(std_results[indx], (std_results[indx].shape[0], 1))
+
+    # We now need to fix the length of the smallest originonally (i.e. min_index)
+
+    # Check if there are different length
+    lengths = [len(x) for x in xs]
+    if not (lengths.count(lengths[0]) == len(lengths) ):
+        # get the unique values of the difference in length.
+        unique_lengths = np.unique(lengths)
+
+        if unique_lengths.size != 2:
+            print("Something is wrong with the lengths of the strings, there should be only two unique lengths")
+            sys.exit(0)
+
+        diff = np.abs(unique_lengths[1] - unique_lengths[0])
+
+        # within all the mins
+        for index in several_min:
+
+            extras = []
+            y_extras = []
+            end = len(xs[index]) - 1
+            for i in range(diff):
+
+                extras.append(xs[index][end - i])
+                y_extras.append(end - i)
+
+            xs[index] = [w for w in xs[index] if w not in extras]
+            ys[index] = np.delete(ys[index], y_extras)
+            ys[index] = np.reshape(ys[index], (ys[index].shape[0], 1))
+
+            std_results[index] = np.delete(std_results[index], y_extras)
+            std_results[index] = np.reshape(std_results[index], (std_results[index].shape[0], 1))
+
+    ### TOP works better #############
+    # # at the end make sure x[min] isn't too big, delete all the ones that were never met.
+    # if counter <= len(xs[min_index]):
+    #     for index in several_min:
+    #
+    #         extras = []
+    #         y_extras = []
+    #         for i in range(counter - 1, len(xs[index])):
+    #             extras.append(xs[index][i])
+    #             y_extras.append(i)
+    #
+    #         xs[index] = [w for w in xs[index] if w not in extras]
+    #         ys[index] = np.delete(ys[index], y_extras)
+    #         ys[index] = np.reshape(ys[index], (ys[index].shape[0], 1))
+    #
+    #         std_results[index] = np.delete(std_results[index], y_extras)
+    #         std_results[index] = np.reshape(std_results[index], (std_results[index].shape[0], 1))
+
+
+    return ys, std_results, xs
+
+def plot(config, metric = "mean_agent_return_with_broadcast_penalties_mean"):
+
+    '''
+    Function to plot results on one plot given a json file specifing the directories and names we would like to use
+
+    :param json_file: Json file of experiment directories and names
+    :param home_directory:  Where we are running the plot script from, this modifies the path to storage directory (We assume everything is being stored in "storage" directory
+    :param x_label: self explanatory
+    :param metric: metric used for the plots compatible with the data inside the pickle file of results
+    :return: saves plot
+    '''
+    json_file = config.json_file
+    home_directory = "."
+    x_label = config.xlabel
+
+    # load json as dict
+    loaded_dict = utils.load_dict_from_json(json_file)
+
+    # get experiments and names
+    experiments = loaded_dict['experiment_folders']
+    names = loaded_dict['names']
+
+    # make sure they are the same length
+    if len(experiments) != len(names):
+        print("Error in number of arguments for experiments or names, make sure there are the same number of experiment folders as there are names")
+        exit(1)
+
+    # create file path to the experiment directories
+    dir = Path(home_directory+'/storage')
+
+    # keep track of xs, mean_results and std dev to be plotted
+    xs = []
+    mean_results = []
+    std_results = []
+    # For each experiment, loops through all seeds as aggregate info
+    for exp in experiments:
+
+        exp_results = []
+        exp_xs = []
+
+        seeds_dir = os.listdir(dir/exp)
+        print(seeds_dir)
+        for seed in seeds_dir:
+            path = os.path.join(dir/exp, seed)
+            print(path)
+            data = utils.load_graph_data(path)[metric]
+            frames = utils.load_graph_data(path)['num_frames']
+            exp_xs.append(frames)
+            exp_results.append(data)
+
+        # make sure each run has same length
+        exp_xs = check_experiment_length(exp_xs)
+        xs.append(exp_xs)
+
+        mean_exp_results = np.mean(exp_results, axis=0)
+        std_exp_results = np.std(exp_results, axis=0)
+
+        mean_results.append(mean_exp_results)
+        std_results.append(std_exp_results)
+
+    pdb.set_trace()
+    # if (check_experiment_length(xs) == False):
+    #     mean_results, std_results, xs = match_length(mean_results, std_results, xs)
+
+    ys = np.concatenate(mean_results, axis= 1).T
+    stds = np.concatenate(std_results, axis=1).T
+
+    #plot everything
+    fig, ax = create_fig((1, 1))
+    plot_curve(ax, xs,
+               ys,
+               stds=stds,
+               labels=names,
+               xlabel=x_label, title=config.title)
+    fig.savefig(config.document_name)
+    plt.close(fig)
+
+
+if __name__=="__main__":
+
+    #home_directory = "."
+    #json_file = "plots/mean_returns_switch_ppo.json"
+    #pdb.set_trace()
+    config = get_plotting_args()
+
+    plot(config)
--- scripts/visualize.py
+++ scripts/visualize.py
@@ -1,11 +1,11 @@
 # #!/usr/bin/env python3
 # USE_TEAMGRID = True
-#
+import pdb
 # import argparse
 # import gym
 # import time
 # from utils.config import parse_bool
-# import imageio
+import imageio
 #
 # import matplotlib.pyplot as plt
 #
@@ -26,6 +26,7 @@ import pdb
 import argparse
 import gym
 import time
+from matplotlib import pyplot as plt
 import logging
 import torch
 import torch_rl
@@ -42,6 +43,7 @@ from make_env import make_env
 
 import utils
 from utils import parse_bool
+from matplotlib import pyplot as plt
 from model import ACModel
 # Parse arguments
 
@@ -58,7 +60,7 @@ parser.add_argument("--env", default=None,
                     help="name of the environment to be run"
                          "if None, env will be taken from saved config.json"
                          "which is the env on which the model was trained")
-parser.add_argument("--num_episodes", type=int, default=30,
+parser.add_argument("--num_episodes", type=int, default=10,
                     help="number of episodes to show")
 parser.add_argument("--shift", type=int, default=0,
                     help="number of times the environment is reset at the beginning (default: 0)")
@@ -91,12 +93,15 @@ train_config = utils.load_config_from_json(filename=dir_manager.seed_dir / "conf
 
 if args.env is None:
     args.env = train_config.env
-
+#pdb.set_trace()
 
 envs =[]
 for i in range(train_config.procs):
     if args.use_teamgrid:
-        env = gym.make(args.env, shared_rewards=args.shared_rewards)
+        if args.env == "TEAMGrid-DualSwitch-v0":
+            env = gym.make(args.env, shared_rewards=args.shared_rewards)
+        else:
+            env = gym.make(args.env, num_goals= train_config.num_goals, num_agents= train_config.num_agents, shared_rewards=args.shared_rewards)
     else:
         env = make_env(train_config.scenario, train_config.benchmark)
     env.seed(args.seed)
@@ -117,7 +122,7 @@ agent = utils.Agent(args.env, env.observation_space, dir_manager.seed_dir, train
 acmodel = agent.acmodel
 preprocess_obss = agent.preprocess_obss
 # Run the agent
-
+pdb.set_trace()
 done = True
 
 i = 0
@@ -136,6 +141,10 @@ while True:
     # renders the environment
 
     if args.save_gifs:
+        #print("HERE")
+        #pdb.set_trace()
+        plt.imshow(env.render('rgb_array'))
+        plt.show()
         frames.append(env.render('rgb_array'))
 
     else:
@@ -144,119 +153,119 @@ while True:
         if elapsed < ifi:
             time.sleep(ifi - elapsed)
 
-        renderer = env.render('human', close=True) # close=False for visualization
+        renderer = env.render('human', close=False) # close=False for visualization
     # action selection
 
     actions = agent.get_action(obss)
-    print('actions', actions)
+    #print('actions', actions)
 
     # environment step
 
     obss, rewards, done, _ = env.step(actions)
-    #print('done', done)
+    print('rewards at timestep', rewards)
 
     for j, reward in enumerate(rewards):
         agent.analyze_feedback(reward, done)
 
 
-    if train_config.algo == "a2c":
-        algo = torch_rl.A2CAlgo(num_agents=train_config.num_agents, envs=envs, acmodel=acmodel, replay_buffer=train_config.replay_buffer, \
-                                num_frames_per_proc=train_config.frames_per_proc, discount=train_config.discount, lr=train_config.lr, \
-                                gae_lambda=train_config.gae_lambda,
-                                entropy_coef=train_config.entropy_coef, value_loss_coef=train_config.value_loss_coef, \
-                                max_grad_norm=train_config.max_grad_norm, recurrence=train_config.recurrence,
-                                rmsprop_alpha=train_config.optim_alpha, rmsprop_eps=train_config.optim_eps, \
-                                preprocess_obss=preprocess_obss, num_options=train_config.num_options)
-    elif train_config.algo == "ppo":
-        algo = torch_rl.PPOAlgo(num_agents=train_config.num_agents, envs=envs, acmodel=acmodel, replay_buffer=train_config.replay_buffer, \
-                                num_frames_per_proc=train_config.frames_per_proc, discount=train_config.discount, lr=train_config.lr, \
-                                gae_lambda=train_config.gae_lambda,
-                                entropy_coef=train_config.entropy_coef, value_loss_coef=train_config.value_loss_coef, max_grad_norm=train_config.max_grad_norm, \
-                                recurrence=train_config.recurrence,
-                                adam_eps=train_config.optim_eps, clip_eps=train_config.clip_eps, epochs=train_config.epochs, batch_size=train_config.batch_size, \
-                                preprocess_obss=preprocess_obss)
-    elif train_config.algo == "oc":
-        algo = torch_rl.OCAlgo(num_agents=train_config.num_agents, envs=envs, acmodel=acmodel, replay_buffer=train_config.replay_buffer, \
-                               num_frames_per_proc=train_config.frames_per_proc, discount=train_config.discount, lr=train_config.lr, \
-                               gae_lambda=train_config.gae_lambda,
-                               entropy_coef=train_config.entropy_coef, value_loss_coef=train_config.value_loss_coef, \
-                               max_grad_norm=train_config.max_grad_norm, recurrence=train_config.recurrence,
-                               rmsprop_alpha=train_config.optim_alpha, rmsprop_eps=train_config.optim_eps, preprocess_obss=preprocess_obss,
-                               num_options=ctrain_config.num_options, termination_loss_coef=train_config.termination_loss_coef, \
-                               termination_reg=train_config.termination_reg)
-    elif train_config.algo == "doc":
-        #config.recurrence = 2
-        algo = torch_rl.DOCAlgo(num_agents=train_config.num_agents, envs=envs, acmodel=acmodel, replay_buffer=train_config.replay_buffer, \
-                                num_frames_per_proc=train_config.frames_per_proc, discount=train_config.discount, lr=train_config.lr, \
-                                gae_lambda=train_config.gae_lambda,
-                               entropy_coef=train_config.entropy_coef, value_loss_coef=train_config.value_loss_coef, max_grad_norm=train_config.max_grad_norm, \
-                                recurrence=train_config.recurrence,
-                               rmsprop_alpha=train_config.optim_alpha, rmsprop_eps=train_config.optim_eps, preprocess_obss=preprocess_obss,
-                               num_options=train_config.num_options, termination_loss_coef=train_config.termination_loss_coef, \
-                                termination_reg=train_config.termination_reg)
-
-    elif train_config.algo == "maddpg":
-        algo = torch_rl.MADDPGAlgo(num_agents=train_config.num_agents, envs=envs, acmodel=acmodel, replay_buffer=train_config.replay_buffer, \
-                                   tau = train_config.tau, num_frames_per_proc=train_config.frames_per_proc, discount=train_config.discount, lr=train_config.lr, \
-                                   gae_lambda=train_config.gae_lambda,
-                 entropy_coef=config.entropy_coef, value_loss_coef=train_config.value_loss_coef, max_grad_norm=train_config.max_grad_norm, \
-                                   recurrence=train_config.recurrence,
-                 adam_eps=train_config.optim_eps, clip_eps=config.clip_eps, epochs=train_config.epochs, er_batch_size=train_config.er_batch_size, \
-                                   preprocess_obss=preprocess_obss)
-
-    #save data
-
-    graph_data = {
-        "num_frames": [],
-        "return_with_broadcast_penalties_mean": [],
-        "return_with_broadcast_penalties_std": [],
-        "mean_agent_return_with_broadcast_penalties_mean": [],
-        "mean_agent_return_with_broadcast_penalties_std": [],
-        "entropy": [],
-        "broadcast_entropy": [],
-        "policy_loss": [],
-        "broadcast_loss": [],
-        "value_loss": [],
-        "options": [],
-        "actions": [],
-        "broadcasts": []
-    }
-
-    logs = algo.update_parameters()
-    #num_frames.append(i)
-    return_per_episode = utils.synthesize(logs["return_per_episode"])
-    return_per_episode_with_broadcast_penalties = utils.synthesize(logs["return_per_episode_with_broadcast_penalties"])
-    mean_agent_return_per_episode_with_broadcast_penalties = utils.synthesize(
-        logs["mean_agent_return_with_broadcast_penalties"])
-    num_frames_per_episode = utils.synthesize(logs["num_frames_per_episode"])
-    options = logs["options"]
-    actions = logs["actions"]
-    broadcasts = logs["broadcasts"]
-
-    #status = {"num_frames": num_frames, "update": update}
-
-    # Saving data for graphs
-
-    graph_data["num_frames"].append(i)
-    #graph_data["return_mean"].append(return_per_episode['mean'])
-    #graph_data["return_std"].append(return_per_episode['std'])
-    graph_data["return_with_broadcast_penalties_mean"].append(return_per_episode_with_broadcast_penalties['mean'])
-    graph_data["return_with_broadcast_penalties_std"].append(return_per_episode_with_broadcast_penalties['std'])
-    graph_data["mean_agent_return_with_broadcast_penalties_mean"].append(
-        mean_agent_return_per_episode_with_broadcast_penalties['mean'])
-    graph_data["mean_agent_return_with_broadcast_penalties_std"].append(
-        mean_agent_return_per_episode_with_broadcast_penalties['std'])
-    #graph_data["episode_length_mean"].append(num_frames_per_episode['mean'])
-    #graph_data["episode_length_std"].append(num_frames_per_episode['std'])
-    graph_data["entropy"].append(logs["entropy"])
-    graph_data["broadcast_entropy"].append(logs["broadcast_entropy"])
-    graph_data["policy_loss"].append(logs["policy_loss"])
-    graph_data["broadcast_loss"].append(logs["broadcast_loss"])
-    graph_data["value_loss"].append(logs["value_loss"])
-    #graph_data["grad_norm"].append(logs["grad_norm"])
-    graph_data["options"].append(options)
-    graph_data["actions"].append(actions)
-    graph_data["broadcasts"].append(broadcasts)
+    # if train_config.algo == "a2c":
+    #     algo = torch_rl.A2CAlgo(num_agents=train_config.num_agents, envs=envs, acmodel=acmodel, replay_buffer=train_config.replay_buffer, \
+    #                             num_frames_per_proc=train_config.frames_per_proc, discount=train_config.discount, lr=train_config.lr, \
+    #                             gae_lambda=train_config.gae_lambda,
+    #                             entropy_coef=train_config.entropy_coef, value_loss_coef=train_config.value_loss_coef, \
+    #                             max_grad_norm=train_config.max_grad_norm, recurrence=train_config.recurrence,
+    #                             rmsprop_alpha=train_config.optim_alpha, rmsprop_eps=train_config.optim_eps, \
+    #                             preprocess_obss=preprocess_obss, num_options=train_config.num_options)
+    # elif train_config.algo == "ppo":
+    #     algo = torch_rl.PPOAlgo(num_agents=train_config.num_agents, envs=envs, acmodel=acmodel, replay_buffer=train_config.replay_buffer, \
+    #                             num_frames_per_proc=train_config.frames_per_proc, discount=train_config.discount, lr=train_config.lr, \
+    #                             gae_lambda=train_config.gae_lambda,
+    #                             entropy_coef=train_config.entropy_coef, value_loss_coef=train_config.value_loss_coef, max_grad_norm=train_config.max_grad_norm, \
+    #                             recurrence=train_config.recurrence,
+    #                             adam_eps=train_config.optim_eps, clip_eps=train_config.clip_eps, epochs=train_config.epochs, batch_size=train_config.batch_size, \
+    #                             preprocess_obss=preprocess_obss)
+    # elif train_config.algo == "oc":
+    #     algo = torch_rl.OCAlgo(num_agents=train_config.num_agents, envs=envs, acmodel=acmodel, replay_buffer=train_config.replay_buffer, \
+    #                            num_frames_per_proc=train_config.frames_per_proc, discount=train_config.discount, lr=train_config.lr, \
+    #                            gae_lambda=train_config.gae_lambda,
+    #                            entropy_coef=train_config.entropy_coef, value_loss_coef=train_config.value_loss_coef, \
+    #                            max_grad_norm=train_config.max_grad_norm, recurrence=train_config.recurrence,
+    #                            rmsprop_alpha=train_config.optim_alpha, rmsprop_eps=train_config.optim_eps, preprocess_obss=preprocess_obss,
+    #                            num_options=ctrain_config.num_options, termination_loss_coef=train_config.termination_loss_coef, \
+    #                            termination_reg=train_config.termination_reg)
+    # elif train_config.algo == "doc":
+    #     #config.recurrence = 2
+    #     algo = torch_rl.DOCAlgo(num_agents=train_config.num_agents, envs=envs, acmodel=acmodel, replay_buffer=train_config.replay_buffer, \
+    #                             num_frames_per_proc=train_config.frames_per_proc, discount=train_config.discount, lr=train_config.lr, \
+    #                             gae_lambda=train_config.gae_lambda,
+    #                            entropy_coef=train_config.entropy_coef, value_loss_coef=train_config.value_loss_coef, max_grad_norm=train_config.max_grad_norm, \
+    #                             recurrence=train_config.recurrence,
+    #                            rmsprop_alpha=train_config.optim_alpha, rmsprop_eps=train_config.optim_eps, preprocess_obss=preprocess_obss,
+    #                            num_options=train_config.num_options, termination_loss_coef=train_config.termination_loss_coef, \
+    #                             termination_reg=train_config.termination_reg)
+    #
+    # elif train_config.algo == "maddpg":
+    #     algo = torch_rl.MADDPGAlgo(num_agents=train_config.num_agents, envs=envs, acmodel=acmodel, replay_buffer=train_config.replay_buffer, \
+    #                                tau = train_config.tau, num_frames_per_proc=train_config.frames_per_proc, discount=train_config.discount, lr=train_config.lr, \
+    #                                gae_lambda=train_config.gae_lambda,
+    #              entropy_coef=config.entropy_coef, value_loss_coef=train_config.value_loss_coef, max_grad_norm=train_config.max_grad_norm, \
+    #                                recurrence=train_config.recurrence,
+    #              adam_eps=train_config.optim_eps, clip_eps=config.clip_eps, epochs=train_config.epochs, er_batch_size=train_config.er_batch_size, \
+    #                                preprocess_obss=preprocess_obss)
+    #
+    # #save data
+    #
+    # graph_data = {
+    #     "num_frames": [],
+    #     "return_with_broadcast_penalties_mean": [],
+    #     "return_with_broadcast_penalties_std": [],
+    #     "mean_agent_return_with_broadcast_penalties_mean": [],
+    #     "mean_agent_return_with_broadcast_penalties_std": [],
+    #     "entropy": [],
+    #     "broadcast_entropy": [],
+    #     "policy_loss": [],
+    #     "broadcast_loss": [],
+    #     "value_loss": [],
+    #     "options": [],
+    #     "actions": [],
+    #     "broadcasts": []
+    # }
+    #
+    # logs = algo.update_parameters()
+    # #num_frames.append(i)
+    # return_per_episode = utils.synthesize(logs["return_per_episode"])
+    # return_per_episode_with_broadcast_penalties = utils.synthesize(logs["return_per_episode_with_broadcast_penalties"])
+    # mean_agent_return_per_episode_with_broadcast_penalties = utils.synthesize(
+    #     logs["mean_agent_return_with_broadcast_penalties"])
+    # num_frames_per_episode = utils.synthesize(logs["num_frames_per_episode"])
+    # options = logs["options"]
+    # actions = logs["actions"]
+    # broadcasts = logs["broadcasts"]
+    #
+    # #status = {"num_frames": num_frames, "update": update}
+    #
+    # # Saving data for graphs
+    #
+    # graph_data["num_frames"].append(i)
+    # #graph_data["return_mean"].append(return_per_episode['mean'])
+    # #graph_data["return_std"].append(return_per_episode['std'])
+    # graph_data["return_with_broadcast_penalties_mean"].append(return_per_episode_with_broadcast_penalties['mean'])
+    # graph_data["return_with_broadcast_penalties_std"].append(return_per_episode_with_broadcast_penalties['std'])
+    # graph_data["mean_agent_return_with_broadcast_penalties_mean"].append(
+    #     mean_agent_return_per_episode_with_broadcast_penalties['mean'])
+    # graph_data["mean_agent_return_with_broadcast_penalties_std"].append(
+    #     mean_agent_return_per_episode_with_broadcast_penalties['std'])
+    # #graph_data["episode_length_mean"].append(num_frames_per_episode['mean'])
+    # #graph_data["episode_length_std"].append(num_frames_per_episode['std'])
+    # graph_data["entropy"].append(logs["entropy"])
+    # graph_data["broadcast_entropy"].append(logs["broadcast_entropy"])
+    # graph_data["policy_loss"].append(logs["policy_loss"])
+    # graph_data["broadcast_loss"].append(logs["broadcast_loss"])
+    # graph_data["value_loss"].append(logs["value_loss"])
+    # #graph_data["grad_norm"].append(logs["grad_norm"])
+    # graph_data["options"].append(options)
+    # graph_data["actions"].append(actions)
+    # graph_data["broadcasts"].append(broadcasts)
     #print("brd", graph_data["broadcasts"])
 
 env.close()
